758e21d128b6b324bb1c0eb034368fdbee9929ac
Qaiser Abbas <qaiserabbas101@gmail.com>
2025-06-13 15:19:11 +0500

    bug fixes

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,15 +6,14 @@ importScripts(
 );
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCADpcuvxbocr_FCDQCW0C60soS6nTrJU4",
-  authDomain: "nextjs-pro-5a934.firebaseapp.com",
-  projectId: "nextjs-pro-5a934",
-  storageBucket: "nextjs-pro-5a934.firebasestorage.app",
-  messagingSenderId: "486147832529",
-  appId: "1:486147832529:web:61112c0fe5cb1847ace3f0",
-  measurementId: "G-GW8QZCL4EN",
-  vapidKey:
-    "BNXsQjGn__umc7FoMmg4lLY1hxzQ38TcR1MycTqTsy6BkZKXt4jGBUQnWyXbMT_u2ea57N5qqsZBj1OXim5UVFA",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+  vapidKey:"",
 });
 
 const messaging = firebase.messaging();
